f0752e09b925215cab499ebbf4e367acc7c034f2
Ibrohim Botirjonov <72242819+Ibrohim229@users.noreply.github.com>
2024-07-16 18:07:26 +0500

    Add files via upload

---
+++ data/index.ts
@@ -0,0 +1,701 @@
+export const services = [
+  {
+    id: 1,
+    icon: "/service1.svg",
+    title: "Team",
+  },
+  {
+    id: 2,
+    icon: "/service2.svg",
+    title: "Development of mobile applications",
+  },
+  {
+    id: 3,
+    icon: "/service3.svg",
+    title: "Development and implementation ERP systems",
+  },
+  {
+    id: 4,
+    icon: "/service4.svg",
+    title: "User interface, User experience design",
+  },
+  {
+    id: 5,
+    icon: "/service5.svg",
+    title: "Optimization IT consulting infrastructure",
+  },
+  {
+    id: 6,
+    icon: "/service6.svg",
+    title: "IT consulting",
+  },
+];
+
+export const platforms = [
+  {
+    id: 1,
+    icon: "/apple.svg",
+    title: "iOS",
+  },
+  {
+    id: 2,
+    icon: "/android.svg",
+    title: "Android",
+  },
+  {
+    id: 3,
+    icon: "/crossplatform.svg",
+    title: "Crossplatform",
+  },
+];
+
+export const technologies = [
+  {
+    id: 1,
+    icon: "/swift.svg",
+    title: "Swift",
+  },
+  {
+    id: 2,
+    icon: "/kotlin.svg",
+    title: "Kotlin",
+  },
+  {
+    id: 3,
+    icon: "/flutter.svg",
+    title: "Flutter",
+  },
+  {
+    id: 4,
+    icon: "/figma.svg",
+    title: "Figma",
+  },
+  {
+    id: 5,
+    icon: "/sketch.svg",
+    title: "Sketch",
+  },
+  {
+    id: 6,
+    icon: "/lottie.svg",
+    title: "Lottie",
+  },
+  {
+    id: 7,
+    icon: "/illustrator.svg",
+    title: "Illustrator",
+  },
+];
+
+export const ITsystems = [
+  {
+    id: 1,
+    icon: "/crm.svg",
+    title: "CRM",
+  },
+
+  {
+    id: 2,
+    icon: "/eLearning.svg",
+    title: "eLearning",
+  },
+  {
+    id: 3,
+    icon: "/eCommerce.svg",
+    title: "E-commerce",
+  },
+  {
+    id: 4,
+    icon: "/POS.svg",
+    title: "POS",
+  },
+  {
+    id: 5,
+    icon: "/sms.svg",
+    title: "Sms / Email",
+  },
+  {
+    id: 6,
+    icon: "/warehouse.svg",
+    title: "Warehouse",
+  },
+];
+
+export const design = [
+  {
+    id: 1,
+    icon: "/ux.svg",
+    title: "UX",
+  },
+  {
+    id: 2,
+    icon: "/ui.svg",
+    title: "UI",
+  },
+  {
+    id: 3,
+    icon: "/prototyping.svg",
+    title: "Prototyping",
+  },
+  {
+    id: 4,
+    icon: "/mobileDesign.svg",
+    title: "Mobile Design",
+  },
+  {
+    id: 5,
+    icon: "/webDesign.svg",
+    title: "Web Design",
+  },
+  {
+    id: 6,
+    icon: "/atomicDesign.svg",
+    title: "Atomic Design",
+  },
+];
+
+export const infrastructure = [
+  {
+    id: 1,
+    icon: "/arch.svg",
+    title: "Architecture",
+  },
+  {
+    id: 2,
+    icon: "/autoTesting.svg",
+    title: "Auto testing",
+  },
+  {
+    id: 3,
+    icon: "/stress.svg",
+    title: "Stress testing",
+  },
+  {
+    id: 4,
+    icon: "/load.svg",
+    title: "Load testing",
+  },
+  {
+    id: 5,
+    icon: "/devops.svg",
+    title: "Devops",
+  },
+  {
+    id: 6,
+    icon: "/cloud.svg",
+    title: "Cloud",
+  },
+  {
+    id: 7,
+    icon: "/cicd.svg",
+    title: "CI / CD",
+  },
+];
+
+export const qualifications = [
+  {
+    id: 1,
+    icon: "/frontend.svg",
+    title: "Frontend",
+  },
+  {
+    id: 2,
+    icon: "/backend.svg",
+    title: "Backend",
+  },
+  {
+    id: 3,
+    icon: "/arch.svg",
+    title: "Architecture",
+  },
+  {
+    id: 4,
+    icon: "/devops.svg",
+    title: "Devops",
+  },
+  {
+    id: 5,
+    icon: "/uxui.svg",
+    title: "UX/UI",
+  },
+  {
+    id: 6,
+    icon: "/qa.svg",
+    title: "QA",
+  },
+];
+
+export const tools = [
+  {
+    id: 1,
+    icon: "/promotheus.svg",
+    title: "Promotheus",
+    usage: "devops",
+  },
+  {
+    id: 2,
+    icon: "/loki.svg",
+    title: "Loki",
+    usage: "devops",
+  },
+  {
+    id: 3,
+    icon: "/bitbucket.svg",
+    title: "Bitbucket",
+    usage: "devops",
+  },
+  {
+    id: 4,
+    icon: "/gitlab.svg",
+    title: "Gitlab",
+    usage: "devops",
+  },
+  {
+    id: 5,
+    icon: "/kotlin.svg",
+    title: "Kotlin",
+    usage: "mobile",
+  },
+  {
+    id: 6,
+    icon: "/android2.svg",
+    title: "Android",
+    usage: "mobile",
+  },
+  {
+    id: 7,
+    icon: "/java.svg",
+    title: "Java",
+    usage: "mobile",
+  },
+  {
+    id: 8,
+    icon: "/azure.svg",
+    title: "Azure",
+    usage: "infrastructure",
+  },
+  {
+    id: 9,
+    icon: "/gcp.svg",
+    title: "GCP",
+    usage: "infrastructure",
+  },
+  {
+    id: 10,
+    icon: "/digitalocean.svg",
+    title: "DigitalOcean",
+    usage: "infrastructure",
+  },
+  {
+    id: 11,
+    icon: "/aws.svg",
+    title: "AWS",
+    usage: "infrastructure",
+  },
+  {
+    id: 12,
+    icon: "/kubernetes.svg",
+    title: "Kubernetes",
+    usage: "infrastructure",
+  },
+  {
+    id: 13,
+    icon: "/elk.svg",
+    title: "ELK",
+    usage: "devops",
+  },
+  {
+    id: 14,
+    icon: "/grafana.svg",
+    title: "Grafana",
+    usage: "devops",
+  },
+  {
+    id: 15,
+    icon: "/terraform.svg",
+    title: "Terraform",
+    usage: "devops",
+  },
+  {
+    id: 16,
+    icon: "/ios.svg",
+    title: "IOS",
+    usage: "mobile",
+  },
+  {
+    id: 17,
+    icon: "/swift.svg",
+    title: "Swift",
+    usage: "mobile",
+  },
+  {
+    id: 18,
+    icon: "/flutter.svg",
+    title: "Flutter",
+    usage: "mobile",
+  },
+  {
+    id: 19,
+    icon: "/next.svg",
+    title: "Next.JS",
+    usage: "frontend",
+  },
+  {
+    id: 20,
+    icon: "/react.svg",
+    title: "React.JS",
+    usage: "frontend",
+  },
+  {
+    id: 21,
+    icon: "/javascript.svg",
+    title: "Javascript",
+    usage: "frontend",
+  },
+  {
+    id: 22,
+    icon: "/balsamic.svg",
+    title: "Balsamic",
+    usage: "ux",
+  },
+  {
+    id: 23,
+    icon: "/invision.svg",
+    title: "Invision",
+    usage: "ux",
+  },
+  {
+    id: 24,
+    icon: "/figma.svg",
+    title: "Figma",
+    usage: "ux",
+  },
+  {
+    id: 25,
+    icon: "/docker.svg",
+    title: "Docker",
+    usage: "devops",
+  },
+  {
+    id: 26,
+    icon: "/postgresql.svg",
+    title: "PostgreSQL",
+    usage: "backend",
+  },
+  {
+    id: 27,
+    icon: "/python.svg",
+    title: "Python",
+    usage: "backend",
+  },
+  {
+    id: 28,
+    icon: "/nodejs.svg",
+    title: "Node.JS",
+    usage: "backend",
+  },
+  {
+    id: 29,
+    icon: "/php.svg",
+    title: "PHP",
+    usage: "backend",
+  },
+  {
+    id: 30,
+    icon: "/go.svg",
+    title: "GO",
+    usage: "backend",
+  },
+  {
+    id: 31,
+    icon: "/ant.svg",
+    title: "Ant Design",
+    usage: "frontend",
+  },
+  {
+    id: 32,
+    icon: "/gatsby.png",
+    title: "Gatsby.JS",
+    usage: "frontend",
+  },
+  {
+    id: 33,
+    icon: "/vue.svg",
+    title: "Vue.JS",
+    usage: "frontend",
+  },
+  {
+    id: 34,
+    icon: "/principle.svg",
+    title: "Principle",
+    usage: "ux",
+  },
+  {
+    id: 35,
+    icon: "/sketch.svg",
+    title: "Sketch",
+    usage: "ux",
+  },
+  {
+    id: 36,
+    icon: "/adobesuite.svg",
+    title: "Adobe suite",
+    usage: "ux",
+  },
+  {
+    id: 37,
+    icon: "/youtrack.svg",
+    title: "YouTrack",
+    usage: "testing",
+  },
+  {
+    id: 38,
+    icon: "/appium.svg",
+    title: "Appium",
+    usage: "testing",
+  },
+  {
+    id: 39,
+    icon: "/cypress.svg",
+    title: "Cypress",
+    usage: "testing",
+  },
+  {
+    id: 40,
+    icon: "/jmeter.svg",
+    title: "Jmeter",
+    usage: "testing",
+  },
+  {
+    id: 41,
+    icon: "/mongodb.svg",
+    title: "MongoDB",
+    usage: "backend",
+  },
+  {
+    id: 42,
+    icon: "/cassandra.svg",
+    title: "Cassandra",
+    usage: "backend",
+  },
+  {
+    id: 43,
+    icon: "/sass.svg",
+    title: "Sass",
+    usage: "frontend",
+  },
+  {
+    id: 44,
+    icon: "/mui.svg",
+    title: "Material UI",
+    usage: "frontend",
+  },
+  {
+    id: 45,
+    icon: "/zeplin.svg",
+    title: "Zeplin",
+    usage: "ux",
+  },
+  {
+    id: 46,
+    icon: "/illustrator.svg",
+    title: "Illustrator",
+    usage: "ux",
+  },
+  {
+    id: 47,
+    icon: "/lottie.svg",
+    title: "Lottie",
+    usage: "ux",
+  },
+  {
+    id: 48,
+    icon: "/photoshop.svg",
+    title: "Photoshop",
+    usage: "ux",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    image: "/akfa.svg",
+  },
+  {
+    id: 2,
+    image: "/cspace.svg",
+  },
+  {
+    id: 3,
+    image: "/delever.svg",
+  },
+  {
+    id: 4,
+    image: "/fonon.svg",
+  },
+  {
+    id: 5,
+    image: "/hamkor.svg",
+  },
+  {
+    id: 6,
+    image: "/hoshimov.svg",
+  },
+  {
+    id: 7,
+    image: "/iman.svg",
+  },
+  {
+    id: 8,
+    image: "/smsuz.svg",
+  },
+  {
+    id: 9,
+    image: "/maxtrack.svg",
+  },
+  {
+    id: 10,
+    image: "/shefburger.svg",
+  },
+  {
+    id: 11,
+    image: "/mytaxi.svg",
+  },
+  {
+    id: 12,
+    image: "/workly.svg",
+  },
+  {
+    id: 13,
+    image: "/rabota.svg",
+  },
+  {
+    id: 14,
+    image: "/goodzone.svg",
+  },
+];
+
+export const servicesNav = [
+  {
+    id: 1,
+    icon: "/navbar/phone.svg",
+    title: "Development of mobile applications",
+  },
+  {
+    id: 2,
+    icon: "/navbar/erp.svg",
+    title: "Development and implementation of ERP systems",
+  },
+  {
+    id: 3,
+    icon: "/navbar/ui.svg",
+    title: "User interface, User experience design",
+  },
+  {
+    id: 4,
+    icon: "/navbar/itcons.svg",
+    title: "IT consulting",
+  },
+  {
+    id: 5,
+    icon: "/navbar/infra.svg",
+    title: "Optimization of IT consulting infrastructure",
+  },
+];
+
+export const portfolio = [
+  {
+    id: 1,
+    icon: "/navbar/delever.svg",
+    title: "Delever",
+  },
+  {
+    id: 2,
+    icon: "/navbar/sms.svg",
+    title: "Sms.uz",
+  },
+  {
+    id: 3,
+    icon: "/navbar/goodzone.svg",
+    title: "Goodzone",
+  },
+  {
+    id: 4,
+    icon: "/navbar/iman.svg",
+    title: "Iman",
+  },
+];
+
+export const language = [
+  {
+    id: 1,
+    icon: "/navbar/rus.svg",
+    title: "Рус",
+  },
+  {
+    id: 2,
+    icon: "/navbar/eng.svg",
+    title: "Eng",
+  },
+];
+
+export const works = [
+  {
+    id: 1,
+    icon: "/website.svg",
+    title: "Website",
+  },
+  {
+    id: 2,
+    icon: "/admin.svg",
+    title: "Admin panel",
+  },
+  {
+    id: 3,
+    icon: "/crossplatform.svg",
+    title: "Crossplatform",
+  },
+  {
+    id: 4,
+    icon: "/webDesign.svg",
+    title: "Web design",
+  },
+  {
+    id: 5,
+    icon: "/mobileDesign.svg",
+    title: "Mobile design",
+  },
+];
+
+export const workingProcess = [
+  {
+    id: 1,
+    icon: "/contact.svg",
+    title: "Contact",
+    subtitle: "Send us your project request or project idea.",
+  },
+  {
+    id: 2,
+    icon: "/analysis.svg",
+    title: "Analysis",
+    subtitle: "We will contact you to clarify your project requirements.",
+  },
+  {
+    id: 3,
+    icon: "/offer.svg",
+    title: "Offer",
+    subtitle: "We will provide you with our free, non-binding application.",
+  },
+  {
+    id: 4,
+    icon: "/team.svg",
+    title: "Team",
+    subtitle: "We provide a team for your requirements.",
+  },
+  {
+    id: 5,
+    icon: "/start.svg",
+    title: "Start",
+    subtitle: "You will get to know the team and we'll get started.",
+  },
+];
